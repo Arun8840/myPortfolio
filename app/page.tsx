c64e3f5550d8cd03278f0db1d1da663ef040c8d4
Arun <arunprakashrani@gmail.com>
2024-11-11 15:59:09 +0530

    New bento grid layout in about page.Contact page flow added.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import About from "../components/About"
+import Contact from "../components/Contact"
 import Homepage from "../components/Home"
 export default function Home() {
   return (
     <>
       <Homepage />
       <About />
+      <Contact />
     </>
   )
 }
---
+++ components/Contact.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Contact() {
+  return (
+    <section className="min-h-screen bg-[#0e0a03] grid place-items-center">
+      <h1 className="text-white text-4xl font-bold">Contact Me</h1>
+    </section>
+  )
+}
+
+export default Contact
