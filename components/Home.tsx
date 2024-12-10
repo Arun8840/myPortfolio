540d83bd48f1ecbafed2a92fbea18b33e95077a4
Arun <arunprakashrani@gmail.com>
2024-12-10 15:45:38 +0530

    build error issue is cleared.new 3d object placed in home page.

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,93 +1,52 @@
 "use client"
 
-import React, { useState } from "react"
-import Button from "@/Utility/UI/Button"
-import CardContainer from "@/Utility/UI/CardContainer"
-import { Bug, Ellipsis, Folder, Home, Info, Phone } from "lucide-react"
-import MenuContainer from "@/Utility/UI/FlotMenu/MenuContainer"
-import { MenuTrigger } from "@/Utility/UI/FlotMenu/MenuTrigger"
-import { MenuItem } from "@/Utility/UI/FlotMenu/MenuItem"
-import { MenuList } from "@/Utility/UI/FlotMenu/MenuList"
+import React, { useRef } from "react"
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import Spline from "@splinetool/react-spline"
+gsap.registerPlugin(useGSAP)
 
 function Homepage() {
-  const [isOpenMenu, setOpenMenu] = useState(false)
-  const MenuItems: { label: string; icon: React.ReactNode }[] = [
-    { label: "Home", icon: <Home size={18} /> },
-    { label: "About", icon: <Info size={18} /> },
-    { label: "Projects", icon: <Folder size={18} /> },
-    { label: "Contact", icon: <Phone size={18} /> },
-  ]
-  const handleOpenMenu = () => {
-    setOpenMenu(!isOpenMenu)
-  }
+  const contentRef = useRef<HTMLDivElement>(null)
+  const splineRef = useRef<HTMLDivElement>(null)
+  useGSAP(() => {
+    gsap.fromTo(
+      [contentRef?.current, splineRef?.current],
+      {
+        opacity: 0,
+      },
+      {
+        stagger: 0.2,
+        delay: 0.5,
+        opacity: 1,
+        duration: 1,
+        ease: "expo.inOut",
+      }
+    )
+  })
   return (
-    <section className="min-h-screen flex flex-col">
+    <section className="min-h-screen flex justify-start p-10">
       {/* Header */}
-      <div className="flex flex-col lg:flex-row gap-3 justify-between container mx-auto items-start py-5">
-        <h1 className="font-mono_normal font-semibold capitalize text-white tracking-wide text-2xl">
+      <div
+        ref={contentRef}
+        className="container flex flex-col gap-4 lg:gap-9  mx-auto"
+      >
+        <h1 className="font-mono_normal font-semibold capitalize text-white text-7xl">
           Im arun
         </h1>
 
-        <p className="lg:w-1/2 text-lg font-mono_bold text-white tracking-wide">
+        <p className=" text-lg font-mono_bold text-white tracking-wide w-full lg:w-1/2">
           A creative Frontend Developer specializing in React, Next.js, and
           modern JavaScript, building stunning and responsive web experiences.
           Let bring your vision to life with clean code and innovative designs.
         </p>
-
-        <div className="hidden lg:flex items-center gap-3">
-          <Button
-            label={"Let's talk"}
-            icon={{
-              value: (
-                <Bug
-                  strokeWidth={0.8}
-                  className="rotate-0 group-hover/buttonTalk:rotate-90 transition-transform duration-150"
-                />
-              ),
-              color: "white",
-            }}
-            key={"contact_button"}
-            varient={"primary"}
-            className="font-mono_normal group/buttonTalk"
-          />
-          <MenuContainer defaultOpen={isOpenMenu}>
-            <MenuTrigger onClick={handleOpenMenu}>
-              <Button
-                className="font-mono_normal"
-                key={"menu_button"}
-                icon={{
-                  color: "white",
-                  value: (
-                    <Ellipsis
-                      size={18}
-                      className="hover:rotate-90 transition-transform duration-200"
-                    />
-                  ),
-                }}
-              />
-            </MenuTrigger>
-            <MenuList className="bg-black">
-              {MenuItems?.map((options, optionIndex: number) => {
-                return (
-                  <MenuItem
-                    onClick={handleOpenMenu}
-                    showIcon={{ show: true, icon: options?.icon }}
-                    className="text-lg text-[var(--primary)]"
-                    key={`${options?.label}-${optionIndex}`}
-                  >
-                    {options?.label}
-                  </MenuItem>
-                )
-              })}
-            </MenuList>
-          </MenuContainer>
-        </div>
       </div>
 
-      {/* Banner */}
-      <CardContainer className="flex-1 hidden lg:block container mx-auto">
-        <h1>content</h1>
-      </CardContainer>
+      <Spline
+        ref={splineRef}
+        className="absolute size-full inset-0"
+        scene="https://prod.spline.design/1quW7DKtLpPPxCmJ/scene.splinecode"
+      />
     </section>
   )
 }
